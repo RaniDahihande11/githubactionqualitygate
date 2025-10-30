36e7e313de45902a5dda86b87babfcf1942c0e0a
RaniDahihande11 <131444270+RaniDahihande11@users.noreply.github.com>
2025-10-30 17:07:38 +0530

    testting1

---
+++ test_calc.py
@@ -0,0 +1,17 @@
+import pytest
+
+from calc import add, sub, mul, div
+
+def test_add():
+    # compare actual o/p and expected o/p
+    assert add(2,3) ==  5
+    # assert will check actual o/p to expected o/p if both are same then will pass test
+
+def test_sub():
+    assert sub(3,1) == 2
+
+def test_mul():
+    assert sub(3,2) == 6
+
+def test_sub():
+    assert sub(4,2) == 2 
